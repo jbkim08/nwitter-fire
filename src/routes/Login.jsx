16d88f66f5b801e4638464036dad1632e01628ef
JB <drv98@naver.com>
2023-12-08 14:10:36 +0900

    로그인 페이지

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import { useState } from 'react';
+import { auth } from '../firebase';
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Error, Form, Input, Switcher, Title, Wrapper } from '../components/auth-components';
 
-const Login = () => {
-  return <div>Login</div>;
-};
+export default function Login() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-export default Login;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setError('');
+    if (isLoading || email === '' || password === '') return;
+    try {
+      setLoading(true);
+      //파이어베이스 로그인
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (e) {
+      if (e instanceof FirebaseError) {
+        setError(e.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>로그인 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name="email"
+          value={email}
+          placeholder="Email"
+          type="email"
+          required
+        />
+        <Input
+          onChange={onChange}
+          value={password}
+          name="password"
+          placeholder="Password"
+          type="password"
+          required
+        />
+        <Input type="submit" value={isLoading ? 'Loading...' : 'Log in'} />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        계정이 없으신가요? <Link to="/create-account">가입하기 &rarr;</Link>
+      </Switcher>
+    </Wrapper>
+  );
+}
