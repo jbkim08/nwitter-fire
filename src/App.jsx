181eb57cb005cdef10fb54c24fae4b99b8c8b14b
JB <drv98@naver.com>
2023-12-08 09:32:28 +0900

    새 라우터 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+import Login from './routes/Login';
+import CreateUser from './routes/CreateUser';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: '/profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-user',
+    element: <CreateUser />,
+  },
+]);
+
 function App() {
-  return <>정리화면</>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
