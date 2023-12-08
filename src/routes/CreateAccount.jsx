3503ff654ac7ec4739d312dc86a78493583c9602
JB <drv98@naver.com>
2023-12-08 14:02:43 +0900

    스타일컴포넌츠 분리

--- src/routes/CreateAccount.jsx
+++ src/routes/CreateAccount.jsx
@@ -1,51 +1,9 @@
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import { Link, useNavigate } from 'react-router-dom';
 import { auth } from '../firebase';
 import { FirebaseError } from 'firebase/app';
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-  margin-bottom: 10px;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { Form, Input, Switcher, Title, Wrapper } from '../components/auth-components';
 
 export default function CreateAccount() {
   const navigate = useNavigate(); //네비객체
@@ -87,7 +45,7 @@ export default function CreateAccount() {
   };
   return (
     <Wrapper>
-      <Title>Log into 𝕏</Title>
+      <Title>가입하기 𝕏</Title>
       <Form onSubmit={onSubmit}>
         <Input
           onChange={onChange}
@@ -116,6 +74,9 @@ export default function CreateAccount() {
         <Input type="submit" value={isLoading ? 'Loading...' : 'Create Account'} />
       </Form>
       {error && <Error>{error}</Error>}
+      <Switcher>
+        이미 계정이 있습니까? <Link to="/login">로그인 &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
