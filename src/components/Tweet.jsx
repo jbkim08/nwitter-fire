8c92998954d7ad1728d6e8c251d742361f95c783
JB <drv98@naver.com>
2023-12-11 11:26:21 +0900

    삭제버튼과 삭제하기

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,4 +1,7 @@
 import { styled } from 'styled-components';
+import { auth, db, storage } from '../firebase';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { deleteObject, ref } from 'firebase/storage';
 
 const Wrapper = styled.div`
   display: grid;
@@ -31,12 +34,43 @@ const Payload = styled.p`
   font-size: 18px;
 `;
 
-export default function Tweet({ username, photo, tweet }) {
+const DeleteButton = styled.button`
+  background-color: tomato;
+  color: white;
+  font-weight: 600;
+  border: 0;
+  font-size: 12px;
+  padding: 5px 10px;
+  text-transform: uppercase;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export default function Tweet({ username, photo, tweet, userId, id }) {
+  const user = auth.currentUser;
+  const onDelete = async () => {
+    const ok = confirm('정말로 이 트윗을 삭제할까요?');
+    if (!ok || user?.uid !== userId) return; //종료
+    try {
+      //db에 트윗 삭제
+      await deleteDoc(doc(db, 'tweets', id)); //트윗 컬렉션의 특정 id문서를 삭제
+      //이미지 삭제
+      if (photo) {
+        const photoRef = ref(storage, `tweets/${user.uid}/${id}`);
+        await deleteObject(photoRef);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Wrapper>
       <Column>
         <Username>{username}</Username>
         <Payload>{tweet}</Payload>
+        <div>
+          {user?.uid === userId ? <DeleteButton onClick={onDelete}>삭제</DeleteButton> : null}
+        </div>
       </Column>
       <Column>{photo ? <Photo src={photo} /> : null}</Column>
     </Wrapper>
